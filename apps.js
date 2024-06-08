2a27177dc239d7938729d36a1f6be41eef5daf6a
Tayyeba Shahid <tayyebashahid01y@gmail.com>
2024-06-08 10:26:13 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,95 @@
+
+let multi=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+console.log(multi[3] [2]);
+
+function students(first,lname,batch){
+this.firstname=first,
+this.lastname=lname,
+this.batch=batch
+}
+
+let student1= new students("tayyeba","shahid","123a7")
+let student2= new students("tayyeba","","")
+
+console.log(student1)
+console.log(student2)
+
+function students(first,lname,batch){
+    this.firstname=first,
+    this.lastname=lname,
+    this.batch=batch
+    thi
+    }
+    
+    let student1= new students("tayyeba","shahid","123a7")
+    let student2= new students("tayyeba","","")
+    
+    console.log(student1)
+    console.log(student2)
+
+function addition(num1,num2){
+    console.log(4 + 6) 
+   return num1 + num2;
+}
+
+
+
+var click= addition(4,6)
+document.write(click)
+
+function check1(){
+    console.log("tayyeba shahid");
+   
+}
+
+function check2(){
+    console.log("nimra shahid");
+    check1();
+}
+
+check2();
+
+
+setInterval(function(){
+    document.write("tayyeba shahid");
+},2000)
+
+setTimeout(function(){
+    document.write("tayyeba shahid");
+},2000)
+
+function addition(num1,num2){
+    alert(num1+num2);
+}
+
+addition(11,8)
+
+function addition(num1,num2){
+    return num1+num2;
+}
+console.log(addition(8,9));
+
+function check1(){
+    console.log("check1 is running");
+}
+
+function check2(){
+    console.log("check2 is running");
+}
+check2();
+
+function students(first,lastname,batch){
+    this.firstname=first,
+    this.lastname=lastname,
+    this.batch=batch
+}
+
+let student1=new students("tayyeba","shahid","A23497")
+let student2=new students("nimra","","")
+
+console.log(student1);
+console.log(student2);
